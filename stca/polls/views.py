787cccf903772c2cc51ccbd0fe1f297400e28375
warrenwang1027 <54422536+warrenwang1027@users.noreply.github.com>
2019-10-11 19:49:45 +0800

    Fix #010003 -- Formatted HTML and Python Files

--- stca/polls/views.py
+++ stca/polls/views.py
@@ -69,97 +69,94 @@ def vote(request, question_id):
 
 
 def mypolls(request):
-  if request.user.is_authenticated:
-    questions = Question.objects.filter(user=request.user).order_by('-created')
-    context = {'questions': questions}
-    return render(request, 'polls/mypolls.html', context)
+    if request.user.is_authenticated:
+        questions = Question.objects.filter(user=request.user).order_by('-created')
+        context = {'questions': questions}
+        return render(request, 'polls/mypolls.html', context)
 
-  return HttpResponse('Unauthorized', status=401)
+    return HttpResponse('Unauthorized', status=401)
 
 def mypolls_create(request):
-  if request.user.is_authenticated:
-    if request.method == 'GET':
-      return render(request, 'polls/mypolls-create.html')
-    elif request.method == 'POST':
-      qtext = request.POST.get('question-text')
-      if not qtext:
-        return HttpResponse("Question does not exist", status=404)
+    if request.user.is_authenticated:
+        if request.method == 'GET':
+            return render(request, 'polls/mypolls-create.html')
+        elif request.method == 'POST':
+            qtext = request.POST.get('question-text')
+        if not qtext:
+            return HttpResponse("Question does not exist", status=404)
       
-      question = Question(user=request.user, question_text=qtext, state='closed')
-      question.save()
+        question = Question(user=request.user, question_text=qtext, state='closed')
+        question.save()
       
-      return HttpResponseRedirect(reverse('polls:mypolls'))
+        return HttpResponseRedirect(reverse('polls:mypolls'))
 
-  return HttpResponse('Unauthorized', status=401)
+    return HttpResponse('Unauthorized', status=401)
 
 def mypolls_update(request, question_id):
-  if request.user.is_authenticated:
-    question = Question.objects.get(pk=question_id)
-    choices = question.choice_set.all()
+    if request.user.is_authenticated:
+        question = Question.objects.get(pk=question_id)
+        choices = question.choice_set.all()
 
-    if not question:
-      return HttpResponse("Question does not exist", status=404)
-
-    if request.method == 'GET':
-      context = {'question': question, 'choices': choices}
-      return render(request, 'polls/mypolls-update.html', context)
-      
-    elif request.method == 'POST':
-      qtext = request.POST.get('question-text')
-      if not qtext:
-        return HttpResponse("Question does not exist", status=404)
-      question.question_text = qtext
-      question.save()
- 
-      for key in request.POST:
-        if key.startswith('choice-text'):
-          text = request.POST.get(key)
-          if not text:
+        if not question:
             return HttpResponse("Question does not exist", status=404)
-          tokens = key.split('-')
-          cid = tokens[2]
-          for choice in choices:
-            if str(choice.id) == cid:
-              choice.choice_text = text
-              choice.save()
-          
-      ctext = request.POST.get('choice-new')
-      if ctext:
-        choice = Choice(user=request.user, question=question, choice_text=ctext)
-        choice.save()
 
-      return HttpResponseRedirect(reverse('polls:mypolls'))
-  
-  return HttpResponse('Unauthorized', status=401)
+        if request.method == 'GET':
+            context = {'question': question, 'choices': choices}
+            return render(request, 'polls/mypolls-update.html', context)
+          
+        elif request.method == 'POST':
+            qtext = request.POST.get('question-text')
+            if not qtext:
+                return HttpResponse("Question does not exist", status=404)
+            question.question_text = qtext
+            question.save()
+    
+            for key in request.POST:
+                if key.startswith('choice-text'):
+                    text = request.POST.get(key)
+                    if not text:
+                        return HttpResponse("Question does not exist", status=404)
+                    tokens = key.split('-')
+                    cid = tokens[2]
+                    for choice in choices:
+                        if str(choice.id) == cid:
+                            choice.choice_text = text
+                            choice.save()
+              
+            ctext = request.POST.get('choice-new')
+            if ctext:
+                choice = Choice(user=request.user, question=question, choice_text=ctext)
+                choice.save()
+
+        return HttpResponseRedirect(reverse('polls:mypolls'))
+      
+    return HttpResponse('Unauthorized', status=401)
 
 def mypolls_delete(request, question_id):
-  if request.user.is_authenticated:
-    if request.method == 'DELETE':
-      question = Question.objects.get(pk=question_id)
-
-      if not question:
-        return JsonResponse({ 'status': '404', 'msg': 'not found'})
-      
-      question.delete()
-      return JsonResponse({ 'status': '200', 'msg': 'success'})
-  
-  return JsonResponse({ 'status': '401', 'msg': 'unauthorized'})
+    if request.user.is_authenticated:
+        if request.method == 'DELETE':
+            question = Question.objects.get(pk=question_id)
+        if not question:
+            return JsonResponse({ 'status': '404', 'msg': 'not found'})
+        question.delete()
+        return JsonResponse({ 'status': '200', 'msg': 'success'})
+    return JsonResponse({ 'status': '401', 'msg': 'unauthorized'})
 
 def mypolls_delete_choice(request, question_id, choice_id):
-  if request.user.is_authenticated:
-    if request.method == 'DELETE':
-      question = Question.objects.get(pk=question_id)
+    if request.user.is_authenticated:
+        if request.method == 'DELETE':
+            question = Question.objects.get(pk=question_id)
 
-      if not question:
-        return JsonResponse({ 'status': '404', 'msg': 'not found'})
+        if not question:
+            return JsonResponse({ 'status': '404', 'msg': 'not found'})
 
-      choices = question.choice_set.all()
-      for choice in choices:
-        if choice.id == choice_id:
-          choice.delete()
+        choices = question.choice_set.all()
+        for choice in choices:
+            if choice.id == choice_id:
+                choice.delete()
       
-      return JsonResponse({ 'status': '200', 'msg': 'success'})
+        return JsonResponse({ 'status': '200', 'msg': 'success'})
   
-  return JsonResponse({ 'status': '401', 'msg': 'unauthorized'})
+    return JsonResponse({ 'status': '401', 'msg': 'unauthorized'})
 
 
